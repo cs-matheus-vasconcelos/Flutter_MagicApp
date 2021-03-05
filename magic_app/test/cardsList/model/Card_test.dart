a23783f0298d7f5b4ed1328926e7719fc56067a0
matheus.de.v.moura <matheus.de.v.moura@accenture.com>
2021-03-05 16:41:46 -0300

    Add decode tests

---
+++ magic_app/test/cardsList/model/Card_test.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:magic_app/cardsList/model/Card.dart';
+import '../../Helpers/Constants/Constants.dart';
+
+void main() {
+
+  group("Card", () {
+    test("init from JSON", () {
+      final card = Card.fromJSON(json.decode('{"name": "A card", "imageUrl": "$testBaseURL"}'));
+      expect(card.name, "A card");
+      expect(card.imageURL, testBaseURL);
+    });
+  });
+
+  group("Deck", () {
+    test("init from JSON", () {
+      final deck = Deck.fromJSON(json.decode('{"cards": [{"name": "A card", "imageUrl": "$testBaseURL"}]}'));
+
+      expect(deck.cards.length, 1);
+    });
+  });
+
+}
