701910b5dde8775349468f095ef380161236b13f
matheus.de.v.moura <matheus.de.v.moura@accenture.com>
2021-03-05 17:53:08 -0300

    Fix test

--- magic_app/test/cardsList/view/CardList_test.dart
+++ magic_app/test/cardsList/view/CardList_test.dart
@@ -3,8 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:magic_app/cardsList/view/CardList.dart';
 import 'package:magic_app/cardsList/viewModel/CardListViewModel.dart';
 import 'package:magic_app/network/NetworkResponse.dart';
+import 'package:magic_app/cardsList/model/Card.dart' as magic;
 import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
+import '../../Helpers/Constants/Constants.dart';
 import '../../Helpers/Utils.dart';
 
 class MockCardListViewModel extends Mock implements CardListViewModel {}
@@ -42,6 +44,23 @@ void main() {
       final textValue = find.text("Error");
 
       expect(textValue, findsOneWidget);
+    });
+
+    testWidgets("Content state", (WidgetTester tester) async {
+      MockCardListViewModel viewModel = MockCardListViewModel();
+      Widget cardList = ChangeNotifierProvider<CardListViewModel>(
+        create: (context) => viewModel,
+        child: CardList(),
+      );
+
+      final List<magic.Card> deck = [];
+      when(viewModel.responseController).thenReturn(Response.completed(deck));
+
+      await tester.pumpWidget(makeTestable(cardList));
+
+      final gridView = find.byType(GridView);
+
+      expect(gridView, findsOneWidget);
     });
   });
 }
